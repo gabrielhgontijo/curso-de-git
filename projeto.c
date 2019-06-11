fb305bed4b17a01f1eaaf36875f6e3335fa4ae7f
Gabriel <gabrielhenrique_kira@hotmail.com>
2019-06-11 16:32:58 -0300

    Adicionando função de multiplicação

--- projeto.c
+++ projeto.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 int subtracao(int a, int b)
 {
 	int sub_num = a-b;
@@ -10,9 +11,15 @@ int soma(int a, int b)
 	int soma_num = a+b;
 	return soma_num;
 }
+int multiplicacao(int a, int b)
+{
+	int mult = a*b;
+	return mult;
+}
 int main()
 {
 	printf("Soma dos numeros: %d\n", soma(3,4));
 	printf("Subtracao dos numeros: %d\n", subtracao(4,3));
+	printf("Multiplicacao dos numeros: %d\n", multiplicacao(2,3));
 	return 0;
 }
